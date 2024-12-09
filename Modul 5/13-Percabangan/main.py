2ecd48f35cf1d4c65f8de601b4bd7104ec07f0b9
Dodystiawan <botakdede009@gmail.com>
2024-12-09 19:29:51 +0700

    Add files via upload

---
+++ Modul 5/13-Percabangan/main.py
@@ -0,0 +1,24 @@
+# percabangan
+# percabangan 1 kondisi
+
+# stuktur percabangan
+
+nama = input("masukan nama anda!")
+
+# percabangan inline
+# if <kondisi> : <aksi>
+if nama == "adam" :print("selamat datang")
+print("Terima Kasih") # bukan aksi
+
+# percabangan insensitas
+if nama == "adam":
+ print("Selamat datang") # aksi 
+print("Selamat belajar") # aksi
+print("Terima Kasih") # bukan aksi
+
+# percabangan dengan Else Statment
+if nama == "adam":
+   print("Selamat datang")
+else:
+    print("Anda Bukan Adam")
+print("Program Berakhir")
---
+++ Modul 5/15-Elif Statment/main.py
@@ -0,0 +1,8 @@
+# membuat kalkulator sederhana
+
+print(*"=")
+print("kalkulator sederhana")
+print(20*"=" + "\n")
+
+angka_1 = float(input{"Bilangan 1 ="})
+operator = input({"Pilih Operator{Hasil = x** *+/- % // x"})
