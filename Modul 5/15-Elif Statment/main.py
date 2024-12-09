5ff9e5dbe035bb29244abfbc25fc43aec39fd1b6
Dodystiawan <botakdede009@gmail.com>
2024-12-09 19:28:36 +0700

    Delete Modul 5 directory

--- Modul 5/15-Elif Statment/main.py
+++
@@ -1,8 +0,0 @@
-# membuat kalkulator sederhana
-
-print(*"=")
-print("kalkulator sederhana")
-print(20*"=" + "\n")
-
-angka_1 = float(input{"Bilangan 1 ="})
-operator = input({"Pilih Operator{Hasil = x** *+/- % // x"})
